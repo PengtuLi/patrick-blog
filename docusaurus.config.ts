28a06d1668d2dab2c31ec3e255d986d6104cf27a
Patrick <lpt373595331@gmail.com>
2025-10-22 23:14:31 +0800

    feat: add LC-232, doc add latex support

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,13 +1,16 @@
-import {themes as prismThemes} from 'prism-react-renderer';
-import type {Config} from '@docusaurus/types';
-import type * as Preset from '@docusaurus/preset-classic';
+import { themes as prismThemes } from "prism-react-renderer";
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
+
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
 
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'Patrick\'s Site',
-  tagline: 'Dinosaurs are cool',
-  favicon: 'img/favicon.ico',
+  title: "Patrick's Site",
+  tagline: "Dinosaurs are cool",
+  favicon: "img/favicon.ico",
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
@@ -15,127 +18,134 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'http://patrick-blog-liard.vercel.app/',
+  url: "http://patrick-blog-liard.vercel.app/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: "facebook", // Usually your GitHub org/user name.
+  projectName: "docusaurus", // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: "throw",
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       {
         docs: {
-          path: 'content/paper', // ✅ 新的文档文件夹路径
-          sidebarPath: './sidebars.ts',     // 侧边栏文件（可以不变，也可以改名）
-          routeBasePath: 'paper',   // ✅ 新的 URL 前缀：不再是 /docs，而是 /documentation
-          editUrl: 'https://github.com/pengtuli/patrick-blog/tree/main/content/paper',
+          path: "content/paper", // ✅ 新的文档文件夹路径
+          sidebarPath: "./sidebars.ts", // 侧边栏文件（可以不变，也可以改名）
+          routeBasePath: "paper", // ✅ 新的 URL 前缀：不再是 /docs，而是 /documentation
+          editUrl:
+            "https://github.com/pengtuli/patrick-blog/tree/main/content/paper",
+          remarkPlugins: [remarkMath],
+          rehypePlugins: [rehypeKatex],
         },
         blog: {
           showReadingTime: true,
           feedOptions: {
-            type: ['rss', 'atom'],
+            type: ["rss", "atom"],
             xslt: true,
           },
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/pengtuli/patrick-blog/tree/main/',
+          editUrl: "https://github.com/pengtuli/patrick-blog/tree/main/",
           // Useful options to enforce blogging best practices
-          onInlineTags: 'warn',
-          onInlineAuthors: 'warn',
-          onUntruncatedBlogPosts: 'warn',
+          onInlineTags: "warn",
+          onInlineAuthors: "warn",
+          onUntruncatedBlogPosts: "warn",
+
+          remarkPlugins: [remarkMath],
+          rehypePlugins: [rehypeKatex],
         },
         theme: {
-          customCss: './src/css/custom.css',
+          customCss: "./src/css/custom.css",
         },
       } satisfies Preset.Options,
     ],
   ],
 
   // add /leetcode and /learning
-    plugins: [
+  plugins: [
     [
-      '@docusaurus/plugin-content-docs',
+      "@docusaurus/plugin-content-docs",
       {
-        id: 'learning',
-        path: 'content/learning',
-        routeBasePath: 'learning', // URL 前缀：/learning/*
-        sidebarPath: './sidebars.ts', // 独立侧边栏
-        editUrl:
-          'https://github.com/pengtuli/patrick-blog/tree/main/content',
+        id: "learning",
+        path: "content/learning",
+        routeBasePath: "learning", // URL 前缀：/learning/*
+        sidebarPath: "./sidebars.ts", // 独立侧边栏
+        editUrl: "https://github.com/pengtuli/patrick-blog/tree/main/content",
+        remarkPlugins: [remarkMath],
+        rehypePlugins: [rehypeKatex],
       },
     ],
     [
-      '@docusaurus/plugin-content-docs',
+      "@docusaurus/plugin-content-docs",
       {
-        id: 'leetcode',
-        path: 'content/leetcode',
-        routeBasePath: 'leetcode', // URL 前缀：/leetcode/*
-        sidebarPath: './sidebars.ts', // 独立侧边栏
-        editUrl:
-          'https://github.com/pengtuli/patrick-blog/tree/main/content',
+        id: "leetcode",
+        path: "content/leetcode",
+        routeBasePath: "leetcode", // URL 前缀：/leetcode/*
+        sidebarPath: "./sidebars.ts", // 独立侧边栏
+        editUrl: "https://github.com/pengtuli/patrick-blog/tree/main/content",
+        remarkPlugins: [remarkMath],
+        rehypePlugins: [rehypeKatex],
       },
     ],
   ],
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: "img/docusaurus-social-card.jpg",
     colorMode: {
       respectPrefersColorScheme: true,
     },
     navbar: {
-      title: 'Patrick\'s Site',
+      title: "Patrick's Site",
       logo: {
-        alt: 'My Site Logo',
-        src: 'img/logo.svg',
+        alt: "My Site Logo",
+        src: "img/logo.svg",
       },
       items: [
         {
-          type: 'docSidebar',
-          sidebarId: 'paperSidebar',
-          position: 'left',
-          label: 'Awesome Paper',
+          type: "docSidebar",
+          sidebarId: "paperSidebar",
+          position: "left",
+          label: "Awesome Paper",
         },
         {
-          type: 'docSidebar',
-          sidebarId: 'learningSidebar',
-          position: 'left',
-          label: 'Learning',
-          docsPluginId: 'learning'
+          type: "docSidebar",
+          sidebarId: "learningSidebar",
+          position: "left",
+          label: "Learning",
+          docsPluginId: "learning",
         },
         {
-          type: 'docSidebar',
-          sidebarId: 'leetcodeSidebar',
-          position: 'left',
-          label: 'Leetcode',
-          docsPluginId: 'leetcode'
+          type: "docSidebar",
+          sidebarId: "leetcodeSidebar",
+          position: "left",
+          label: "Leetcode",
+          docsPluginId: "leetcode",
         },
-        {to: '/blog', label: 'Blog', position: 'left'},
+        { to: "/blog", label: "Blog", position: "left" },
         {
-          href: 'https://github.com/pengtuli/patrick-blog',
-          label: 'GitHub',
-          position: 'right',
+          href: "https://github.com/pengtuli/patrick-blog",
+          label: "GitHub",
+          position: "right",
         },
       ],
     },
     footer: {
-      style: 'dark',
+      style: "dark",
       // links: [
       //   {
       //     title: 'Docs',
@@ -167,6 +177,15 @@ const config: Config = {
       darkTheme: prismThemes.dracula,
     },
   } satisfies Preset.ThemeConfig,
+  stylesheets: [
+    {
+      href: "https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css",
+      type: "text/css",
+      integrity:
+        "sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM",
+      crossorigin: "anonymous",
+    },
+  ],
 };
 
 export default config;
